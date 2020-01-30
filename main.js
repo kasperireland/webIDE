9ab69c6438e63f18ef1d8f9697d580124f852b63
Shivam Agrawal <shivam301296@gmail.com>
2020-01-30 21:02:41 +0530

    updated

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 var editor = ace.edit("editor");
 //editor.setTheme("ace/theme/github");
 //editor.session.setMode("ace/mode/javascript");
+let fontSize = 18;
+
 editor.setOptions({
 	fontFamily: "monospace",
-	fontSize: "14pt",
+	fontSize: fontSize,
 	highlightActiveLine: false,
 	highlightGutterLine: true,
 	enableBasicAutocompletion: true,
@@ -11,7 +13,7 @@ editor.setOptions({
 });
 
 // var EditSession = require("ace/edit_session").EditSession;
-var EditSession= ace.EditSession;
+var EditSession = ace.EditSession;
 
 var html = new EditSession("<h2>Hello World</h2>");
 html.setMode("ace/mode/html");
@@ -25,7 +27,7 @@ css.on("change", function(delta) {
 	updateCss();
 });
 
-var js = new EditSession("console.log('Hello World')");
+var js = new EditSession("console.log('Hello World');");
 js.setMode("ace/mode/javascript");
 js.on("change", function(delta) {
 	updateJs();
@@ -39,16 +41,25 @@ function updateHtml() {
 }
 
 function updateCss() {
-	$('#runI').contents().find('style').remove();
+	$("#runI")
+		.contents()
+		.find("style")
+		.remove();
 	let doc = runI.contentDocument;
 	doc.body.innerHTML =
 		doc.body.innerHTML + "<style>" + css.getValue() + "</style>";
 }
 
 function updateJs() {
-	$('#runI').contents().find('script').remove();
-	var scriptTag = "<script>"+js.getValue()+"</script>";
-	$("#runI").contents().find("body").append(scriptTag);
+	$("#runI")
+		.contents()
+		.find("script")
+		.remove();
+	var scriptTag = "<script>" + js.getValue() + "</script>";
+	$("#runI")
+		.contents()
+		.find("body")
+		.append(scriptTag);
 }
 
 //=======================================================
@@ -70,32 +81,60 @@ function setJs() {
 }
 
 //==============================================
-function getValue(msg){
+function getValue(msg) {
 	return prompt(msg, "");
 }
 //==============================================
-function downloadProject(){
-	let htmlC= html.getValue();
-	let cssC= css.getValue();
-	let jsC= js.getValue();
+function downloadProject() {
+	let htmlC = html.getValue();
+	let cssC = css.getValue();
+	let jsC = js.getValue();
 
-	let projectName= $("#title").val();
-	if(projectName==="Enter Project Name"){
-		projectName= getValue("Please Enter Project Title:");
+	let projectName = $("#title").val();
+	if (projectName === "Enter Project Name") {
+		projectName = getValue("Please Enter Project Title:");
+		$("#title").val(projectName);
+	}
+	if (!projectName || projectName === "") {
+		projectName = "Unnamed Project";
 		$("#title").val(projectName);
 	}
 
-	let zip= new JSZip();
+	let zip = new JSZip();
 	zip.file("index.html", htmlC);
-	let cssFolder= zip.folder("css");
+	let cssFolder = zip.folder("css");
 	cssFolder.file("main.css", cssC);
-	let jsFolder= zip.folder("js");
+	let jsFolder = zip.folder("js");
 	jsFolder.file("main.js", jsC);
-	zip.generateAsync({
-		type: "blob"
-	}).then(function(content){
-		saveAs(content, projectName+".zip");
-	})
+	zip
+		.generateAsync({
+			type: "blob"
+		})
+		.then(function(content) {
+			saveAs(content, projectName + ".zip");
+		});
 }
 
 //downloadProject();
+//============================================
+
+$("#menuUl").hide();
+function showMenu() {
+	$("#menuUl").toggle();
+}
+
+function hideMenu() {
+	$("#menuUl").hide();
+}
+
+$("#zoomInB").click(function() {
+	fontSize = fontSize + 4;
+	editor.setFontSize(fontSize);
+	hideMenu();
+});
+
+$("#zoomOutB").click(function() {
+	fontSize = fontSize - 4;
+	editor.setFontSize(fontSize);
+	hideMenu();
+});
